65af054df13bd20901824a0095bfe4f1acbbb9c5
Jaime Deng <jaime@Jaimes-MacBook-Pro.local>
2020-03-26 00:37:04 -0700

    action testing

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const converter = (num) => {
     if (isNaN(num))
         return "Enter an integer";
 
-        else if (num == ""){
-            return "No value entered"
-        }
+        // else if (num == ""){
+        //     return "No value entered"
+        // }
 
         else if (num <= 0) {
             return "Negative roman numerals do not exist"
